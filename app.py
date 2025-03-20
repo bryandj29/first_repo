1f7fe4b516c9f6322d5a0c20f9b6bc4e4f51ee22
Bryan Delgado <bryandj84@gmail.com>
2025-03-20 14:36:05 +0000

    exp puerto 8080

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +7,5 @@ def hello():
     return "¡hola mundo, soy Bryan Delgado Justiz!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    port = int(os.getenv("PORT", 8080))  
+    app.run(host="0.0.0.0", port=port)
