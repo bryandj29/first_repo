cc5554b6e7d64d9dd24eceb405d650d0eb6fee60
Bryan Delgado <bryandj84@gmail.com>
2025-03-20 19:31:33 +0000

    proyecto finalizado env-var

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "¡hola mundo, soy Bryan Delgado Justiz!"
 
+@app.route('/version')
+def version():
+    return jsonify({"version": os.getenv("APP_VERSION", "No definida")})
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8080))  
     app.run(host="0.0.0.0", port=port)
